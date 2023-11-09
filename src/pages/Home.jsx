6f11a4f39d32a5ab2122f8f73ac2d3314184711a
Santiago Cano <sanblackx1@gmail.com>
2023-11-09 03:06:33 -0500

    NEW: Font Montserrat added to tw

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,31 +8,27 @@ const LayoutHome = () => {
       <section className="marcas">
         <div className="marcas__container">
           <div className="encabezado">
-            <h2>Fácil y cómodo</h2>
-            <p>selecciona dos de los mercados pradeños </p>
-            <p>para comparar sus precios:</p>
+            <h2 className="font-Montserrat font-bold">Fácil y cómodo</h2>
+            <p>Empieza seleccionando uno de los mercado pradeños </p>
+            <p>para buscar y comparar productos</p>
           </div>
           <div className="imagenes">
             <Link to="/vaquita" className="shadow-lg right-3 rounded-full">
-              <img src="../../public/imagenes/vaquita.jpg" alt="vaquita" />
+              <img src="/imagenes/vaquita.jpg" alt="vaquita" />
             </Link>
             <Link to="/d1" className="shadow-lg right-3 rounded-full">
-              <img src="../public/imagenes/D1.jpg" alt="d1" className="img2" />
+              <img src="/imagenes/D1.jpg" alt="d1" className="img2" />
             </Link>
             <Link to="/isimo" className="shadow-lg right-3 rounded-full">
-              <img src="../public/imagenes/Ísimo.jpg" alt="isimo" />
+              <img src="/imagenes/Ísimo.jpg" alt="isimo" />
             </Link>
             <Link to="/mercaprado" className="shadow-lg right-3 rounded-full">
-              <img src="../public/imagenes/Mercaprado.jpg" alt="mercaprado" />
+              <img src="/imagenes/Mercaprado.jpg" alt="mercaprado" />
             </Link>
             <Link to="/maxioferta" className="shadow-lg right-3 rounded-full">
-              <img src="../../public/imagenes/Maxioferta.jpg" alt="oferta" />
+              <img src="/imagenes/Maxioferta.jpg" alt="oferta" />
             </Link>
           </div>
-        </div>
-
-        <div className="comparar">
-          <a href="#">vamos a comparar</a>
         </div>
       </section>
     </>
