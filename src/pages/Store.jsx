e6ef3f6ee6c2ff3071fd5032bee1cbf30834f0de
Santiago Cano <sanblackx1@gmail.com>
2023-11-11 00:15:48 -0500

    NEW: Pagina para cada buscar en las tiendas

---
+++ src/pages/Store.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
+const Store = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Store;
