acaba5f7d4bfc59910ce31063869778013b6196e
Santiago Cano <sanblackx1@gmail.com>
2023-11-12 03:34:13 -0500

    NEW: Manejando el formulario de busqueda

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 const SearchBar = () => {
+  const searchText = React.useRef();
+  /**
+   * Function that triggers the Backend API
+   * @param {React.FormEvent} evt
+   */
+  const handleSubmit = (evt) => {
+    // evt.preventDefault();
+    console.log(evt.target);
+    console.log(searchText.current.value);
+  };
   return (
     <form
-      action="/"
+      action="products"
       method="get"
       className="flex justify-center items-center gap-1 mx-auto my-0 w-full px-6 sm:max-w-2xl"
+      onSubmit={handleSubmit}
     >
       <input
         type="text"
         name="q"
         id="q"
-        autoFocus="true"
-        placeholder="buscar"
-        className="bg-gray-200/10 shadow-md p-2 rounded-lg w-[inherit] ring-indigo-400 ring-1"
+        autoFocus={true}
+        autoComplete="false"
+        placeholder="Buscar producto"
+        className="bg-gray-200/10 shadow-lg p-2 rounded-lg w-[inherit] ring-indigo-400 ring-1"
+        ref={searchText}
       />
       <button type="submit">
         <MagnifyingGlassIcon className="w-8 h-10 text-indigo-500" />
