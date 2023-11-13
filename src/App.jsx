785490cfa117f1b51988906af133e333bc8f34af
Santiago Cano <sanblackx1@gmail.com>
2023-11-13 01:17:25 -0500

    ADD: Aprendiendo a realizar el redireccionamiento de paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Compare from './components/Compare';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Layout from './pages/Layout';
-import NotFound from './pages/NotFound';
-import LayoutNavigation from './pages/Layout/Navigation';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
 import Store from './pages/Store';
+import NotFound from './pages/NotFound';
+import Compare from './components/Compare';
 import GridProducts from './pages/Layout/GridProducts';
+import LayoutNavigation from './pages/Layout/Navigation';
 
 const requested = async ({ params, request }) => {
   console.log({ params, request });
@@ -13,17 +14,15 @@ const requested = async ({ params, request }) => {
 const App = () => {
   return (
     <div className="relative h-[100dvh] bg-slate-300/10 overflow-x-hidden">
-      <Router>
-        <LayoutNavigation />
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/compare" element={<Compare />} />
-          <Route path="/search/:store" element={<Store />}>
-            <Route path="products" action={requested} element={<GridProducts />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <LayoutNavigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/:store" element={<Store />}>
+          <Route path="products" action={requested} element={<GridProducts />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
