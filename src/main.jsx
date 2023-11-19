62fbc40ad9a58a990b7bf130684a3c5596a27092
Santiago Cano <sanblackx1@gmail.com>
2023-11-19 03:13:50 -0500

    ADD: Solo se renderiza la app desde main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import { RouterProvider } from 'react-router-dom';
+import { configRouter } from './routes/index';
+
 import './index.css';
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={configRouter} />
   </React.StrictMode>
 );
