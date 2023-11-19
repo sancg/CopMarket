796a248eb1e581e982ba7512a4f364622c66c689
Santiago Cano <sanblackx1@gmail.com>
2023-11-18 23:21:47 -0500

    IMPROVE: Simplifica la extracion dependiendo de la tienda disponible

--- backend/controllers/vendor.js
+++ backend/controllers/vendor.js
@@ -1,10 +1,21 @@
-const fs = require('node:fs/promises');
-const { extname, join } = require('node:path');
+const { cacheFile } = require('./cacheProduct');
 
 const supportedVendors = {
-  d1: {},
-  isimo: false,
-  vaquita: false
+  d1: {
+    getProduct: async function (prodQuery) {
+      const { ExtractD1 } = await import('../services/ExtractD1.mjs');
+      const products = await ExtractD1(prodQuery);
+      return products;
+    }
+  },
+  vaquita: {
+    getProduct: async function (prodQuery) {
+      const { ExtractVaquita } = await import('../services/ExtractVaquita.mjs');
+      const products = await ExtractVaquita(prodQuery);
+      return products;
+    }
+  },
+  isimo: false
 };
 
 /**
@@ -30,48 +41,17 @@ const getVendor = (req, res, next) => {
  */
 const getVendorProduct = async (req, res, next) => {
   const { name, query } = req.params;
-  const { ExtractD1 } = await import('../services/ExtractD1.mjs');
 
   if (!supportedVendors[name]) {
     return res.status(404).json(`Tienda ${name} no encontrada`);
   }
 
-  const { storagePath } = await import('../utils/rootDir.mjs');
+  const hasCache = await cacheFile(name, query);
+  console.log(hasCache);
+  if (hasCache) return res.json(hasCache);
 
-  try {
-    await fs.access(storagePath);
-  } catch (error) {
-    console.log('Creating Storage Folder');
-    await fs.mkdir(storagePath, { recursive: true });
-    console.log(error.stack);
-  }
-
-  const files = await fs.readdir(storagePath);
-  const cacheFiles = files.map((f) => {
-    if (extname(f) === '.json') {
-      return f;
-    }
-  });
-
-  const cacheRegEx = new RegExp(`${name}\_${query}`, 'i');
-
-  const foundFile = cacheFiles.find((f) => cacheRegEx.test(f));
-  if (foundFile) {
-    const resFile = JSON.parse(
-      await fs.readFile(join(storagePath, foundFile), { encoding: 'utf8' })
-    );
-    console.log('\x1b[33mProduct cached\x1b[0m');
-    return res.json(resFile);
-  }
-
-  // console.log({ name, query, cacheFiles, foundFile });
-
-  // return res.json('ok');
-  ExtractD1(query)
-    .then((prods) => {
-      res.json(prods);
-    })
-    .catch((e) => console.log(e));
+  const returnProducts = await supportedVendors[name].getProduct(query);
+  res.json(returnProducts);
 };
 
 module.exports = {
